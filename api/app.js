6f69b9f2cb434c112d1d712ce9d5ee9890e90c8d
fedor nikitin <f.a.nikitin@gmail.com>
2024-09-24 02:09:49 +0300

    toimiva frontend

--- api/app.js
+++ api/app.js
@@ -6,6 +6,14 @@ const fs = require("fs");
 var app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // GET-pyyntö "/words"-reitille
 app.get("/words", function (req, res) {
